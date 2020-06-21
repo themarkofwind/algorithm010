74c579d623448d7e49216af7ce67b0c057470410
themarkofwind <themarkofwind@163.com>
2020-06-21 18:26:12 +0800

    add ugly number optimize

--- Week02/UglyNumber.java
+++ Week02/UglyNumber.java
@@ -21,7 +21,7 @@ class UglyNumber {
     }
 
     // 用set去重，减少heap中的查找时间
-    public int nthUglyNumber(int n) {
+    public int nthUglyNumber2(int n) {
         if (n <= 0) return -1;
         PriorityQueue<Long> heap = new PriorityQueue<>();
         Set<Long> uglySet = new HashSet<>();
@@ -45,6 +45,32 @@ class UglyNumber {
                 heap.offer(min * 5);
                 uglySet.add(min * 5);
             }
+        }
+    }
+
+    // 堆中数据不去重
+    // 每次获取堆顶元素时，对比相邻元素是否相同
+    // 忽略相同的丑数
+    public int nthUglyNumber(int n) {
+        if (n <= 0) return -1;
+        PriorityQueue<Long> heap = new PriorityQueue<>();
+        heap.offer(1L);
+        long cur = 0L;
+        while (true) {
+            if (cur != heap.peek()) {
+                cur = heap.poll();
+                n--;
+                heap.offer(cur * 2);
+                heap.offer(cur * 3);
+                heap.offer(cur * 5);
+            } else {
+                // 堆顶元素已与上一个丑数相同
+                heap.poll();
+            }
+
+            if (n == 0) {
+                return (int) cur;
+            }            
         }
     }
 }
