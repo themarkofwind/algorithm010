90c9121932cb97a8f157d07e848ed0ddbb3884a3
themarkofwind <themarkofwind@163.com>
2020-08-04 00:31:01 +0800

    add ultimate dp leetcode 72

---
+++ Week09/[72]Edit Distance.java
@@ -0,0 +1,68 @@
+//Given two words word1 and word2, find the minimum number of operations require
+//d to convert word1 to word2. 
+//
+// You have the following 3 operations permitted on a word: 
+//
+// 
+// Insert a character 
+// Delete a character 
+// Replace a character 
+// 
+//
+// Example 1: 
+//
+// 
+//Input: word1 = "horse", word2 = "ros"
+//Output: 3
+//Explanation: 
+//horse -> rorse (replace 'h' with 'r')
+//rorse -> rose (remove 'r')
+//rose -> ros (remove 'e')
+// 
+//
+// Example 2: 
+//
+// 
+//Input: word1 = "intention", word2 = "execution"
+//Output: 5
+//Explanation: 
+//intention -> inention (remove 't')
+//inention -> enention (replace 'i' with 'e')
+//enention -> exention (replace 'n' with 'x')
+//exention -> exection (replace 'n' with 'c')
+//exection -> execution (insert 'u')
+// 
+// Related Topics String Dynamic Programming 
+// 👍 4093 👎 57
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    // dp formula:
+    // if word1[i-1] == word2[j-1]: dp[i][j] = dp[i-1][j-1]
+    // else: dp[i][j] = min(dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + 1)
+    public int minDistance(String word1, String word2) {
+        if (null == word1 && null == word2) {
+            return 0;
+        } else if (null == word1) {
+            return word2.length();
+        } else if (null == word2) {
+            return word1.length();
+        }
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) dp[i][0] = i;
+        for (int j = 0; j <= word2.length(); j++) dp[0][j] = j;
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j] + 1, Math.min(dp[i][j-1] + 1, dp[i-1][j-1] + 1));
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
